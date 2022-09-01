470a4fb1587a2259bd6d9dcbdd791f0736288eb5
Lucas Silva de Oliveira <lukbrowww@hotmail.com>
2022-08-31 23:22:24 -0300

    Adição dos Métodos de Busca em lista de consultor, e aplicação do botão de busca na tela de lista de consultores

--- src/cons/visao/TelaListaDeConsultor.java
+++ src/cons/visao/TelaListaDeConsultor.java
@@ -39,13 +39,11 @@ public class TelaListaDeConsultor extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         jTableProjetos = new javax.swing.JTable();
         jButtonHome = new javax.swing.JButton();
-        jButton1 = new javax.swing.JButton();
+        jButtonBuscar = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
-        jLabel3 = new javax.swing.JLabel();
-        jTextField2 = new javax.swing.JTextField();
+        jTextFieldNome = new javax.swing.JTextField();
         jLabel4 = new javax.swing.JLabel();
-        jTextField3 = new javax.swing.JTextField();
+        jTextFieldID = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Lista de Consultores");
@@ -100,14 +98,17 @@ public class TelaListaDeConsultor extends javax.swing.JFrame {
             }
         });
 
-        jButton1.setBackground(new java.awt.Color(255, 255, 255));
-        jButton1.setFont(new java.awt.Font("Courier New", 1, 14)); // NOI18N
-        jButton1.setForeground(new java.awt.Color(0, 0, 0));
-        jButton1.setText("Buscar");
+        jButtonBuscar.setBackground(new java.awt.Color(255, 255, 255));
+        jButtonBuscar.setFont(new java.awt.Font("Courier New", 1, 14)); // NOI18N
+        jButtonBuscar.setForeground(new java.awt.Color(0, 0, 0));
+        jButtonBuscar.setText("Buscar");
+        jButtonBuscar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonBuscarActionPerformed(evt);
+            }
+        });
 
         jLabel2.setText("Nome");
-
-        jLabel3.setText("Telefone");
 
         jLabel4.setText("ID");
 
@@ -128,17 +129,13 @@ public class TelaListaDeConsultor extends javax.swing.JFrame {
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jLabel4)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jTextField3)
+                        .addComponent(jTextFieldID)
                         .addGap(18, 18, 18)
                         .addComponent(jLabel2)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 382, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jTextFieldNome, javax.swing.GroupLayout.PREFERRED_SIZE, 614, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(18, 18, 18)
-                        .addComponent(jLabel3)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 159, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
-                        .addComponent(jButton1)))
+                        .addComponent(jButtonBuscar)))
                 .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
@@ -148,13 +145,11 @@ public class TelaListaDeConsultor extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButtonListar, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton1)
+                    .addComponent(jButtonBuscar)
                     .addComponent(jLabel2)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel3)
-                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jTextFieldNome, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel4)
-                    .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jTextFieldID, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane1)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -178,9 +173,9 @@ public class TelaListaDeConsultor extends javax.swing.JFrame {
 
     private void jButtonListarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonListarActionPerformed
         try {
-            ConsultorDAO dadosBD = new ConsultorDAO();
+            ConsultorDAO bd = new ConsultorDAO();
             ArrayList<Consultor> dadosDosConsultores = null;
-            dadosDosConsultores = dadosBD.listagemDeConsultores();
+            dadosDosConsultores = bd.listagemDeConsultores();
             DefaultTableModel model =  (DefaultTableModel) jTableProjetos.getModel();
             model.setNumRows(0);
             for(int pos = 0; pos < dadosDosConsultores.size(); pos++){
@@ -206,6 +201,53 @@ public class TelaListaDeConsultor extends javax.swing.JFrame {
         telaAcesso.setVisible(true);
         dispose();
     }//GEN-LAST:event_jButtonHomeActionPerformed
+
+    private void jButtonBuscarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonBuscarActionPerformed
+        try {
+            ConsultorDAO bd = new ConsultorDAO();
+            if( !"".equals(jTextFieldID.getText())){
+                int id_consultor = Integer.parseInt(jTextFieldID.getText());
+                ArrayList<Consultor> dadosDosConsultores = null;
+                dadosDosConsultores = bd.consultaListaConsultoresID(id_consultor);
+                DefaultTableModel model =  (DefaultTableModel) jTableProjetos.getModel();
+                model.setNumRows(0);
+                for(int pos = 0; pos < dadosDosConsultores.size(); pos++){
+                    Consultor objetoConsultor = dadosDosConsultores.get(pos);
+                    String[] linha = new String[8];
+                    linha[0]= objetoConsultor.getId_Consultor()+ "";
+                    linha[1]= objetoConsultor.getNome_Consultor();
+                    linha[2]= objetoConsultor.getTelefone_Consultor_1();
+                    linha[3]= objetoConsultor.getTelefone_Consultor_2();
+                    linha[4]= objetoConsultor.getTelefone_Consultor_3();
+                    linha[5]= objetoConsultor.getEmail_Consultor_1();
+                    linha[6]= objetoConsultor.getEmail_Consultor_2();
+                    linha[7]= objetoConsultor.getEmail_Consultor_3();
+                    model.addRow(linha);
+            }} 
+            else if( !"".equals(jTextFieldNome.getText())) {
+                String nome_consultor = jTextFieldNome.getText();
+                Consultor consultor = new Consultor(0, nome_consultor, "","","","","","");
+                ArrayList<Consultor> dadosDosConsultores = null;
+                dadosDosConsultores = bd.consultaListaConsultoresNome(consultor);
+                DefaultTableModel model =  (DefaultTableModel) jTableProjetos.getModel();
+                model.setNumRows(0);
+                for(int pos = 0; pos < dadosDosConsultores.size(); pos++){
+                    Consultor objetoConsultor = dadosDosConsultores.get(pos);
+                    String[] linha = new String[8];
+                    linha[0]= objetoConsultor.getId_Consultor()+ "";
+                    linha[1]= objetoConsultor.getNome_Consultor();
+                    linha[2]= objetoConsultor.getTelefone_Consultor_1();
+                    linha[3]= objetoConsultor.getTelefone_Consultor_2();
+                    linha[4]= objetoConsultor.getTelefone_Consultor_3();
+                    linha[5]= objetoConsultor.getEmail_Consultor_1();
+                    linha[6]= objetoConsultor.getEmail_Consultor_2();
+                    linha[7]= objetoConsultor.getEmail_Consultor_3();
+                    model.addRow(linha);       
+            }}
+        } catch (Exception erro) {
+            JOptionPane.showMessageDialog(rootPane, erro.getMessage());
+        }
+    }//GEN-LAST:event_jButtonBuscarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -244,19 +286,17 @@ public class TelaListaDeConsultor extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButtonBuscar;
     private javax.swing.JButton jButtonHome;
     private javax.swing.JButton jButtonListar;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTableProjetos;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
+    private javax.swing.JTextField jTextFieldID;
+    private javax.swing.JTextField jTextFieldNome;
     // End of variables declaration//GEN-END:variables
 }
